cc9ff421c904129208fe27d73460407177d0c013
ray-seal <rayseal1986@gmail.com>
2026-09-11 22:18:23 +0100

    Updated Saintburied app

--- Store3D/ShopPhone.gd
+++ Store3D/ShopPhone.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 
 @export var chocolate_product: Product
 @export var cola_product: Product
+@export var crisps_product: Product
 
 func _on_chocolate_plus_pressed() -> void:
 	var amount = $PhonePanel/ProductScroll/ProductList/ChocolateCard/ChocolateAmount
@@ -20,11 +21,13 @@ func _on_chocolate_minus_pressed() -> void:
 func update_order_total() -> void:
 	var chocolate_amount = int($PhonePanel/ProductScroll/ProductList/ChocolateCard/ChocolateAmount.text)
 	var cola_amount = int($PhonePanel/ProductScroll/ProductList/ColaCard/ColaAmount.text)
+	var crisps_amount = int($PhonePanel/ProductScroll/ProductList/CrispsCard/CrispsAmount.text)
 	
 	var chocolate_total = chocolate_amount * chocolate_product.wholesale_cost
 	var cola_total = cola_amount * cola_product.wholesale_cost
+	var crisps_total = crisps_amount * crisps_product.wholesale_cost
 	
-	var total = chocolate_total + cola_total
+	var total = chocolate_total + cola_total + crisps_total
 	
 	$PhonePanel/OrderTotal.text = "TOTAL: £%.2f" % total
 	
@@ -45,7 +48,50 @@ func _on_cola_minus_pressed() -> void:
 	
 func _ready() -> void:
 	visible = false
+	$PhonePanel/PlaceOrderButton.text = "PLACE ORDER"
+	$PhonePanel/PlaceOrderButton.disabled = false
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed and event.keycode == KEY_P:
 		visible = not visible
+
+		if visible:
+			$PhonePanel/PlaceOrderButton.text = "PLACE ORDER"
+			$PhonePanel/PlaceOrderButton.disabled = false
+
+func _on_crisps_plus_pressed() -> void:
+	var amount = $PhonePanel/ProductScroll/ProductList/CrispsCard/CrispsAmount
+	amount.text = str(int(amount.text) + 1)
+	update_order_total()
+
+
+func _on_crisps_minus_pressed() -> void:
+	var amount = $PhonePanel/ProductScroll/ProductList/CrispsCard/CrispsAmount
+	var current = int(amount.text)
+	
+	if current > 0:
+		amount.text = str(current - 1)
+		
+	update_order_total()
+
+
+func _on_place_order_button_pressed() -> void:
+	var chocolate_amount = int($PhonePanel/ProductScroll/ProductList/ChocolateCard/ChocolateAmount.text)
+	var cola_amount = int($PhonePanel/ProductScroll/ProductList/ColaCard/ColaAmount.text)
+	var crisps_amount = int($PhonePanel/ProductScroll/ProductList/CrispsCard/CrispsAmount.text)
+	
+	print("ORDER PLACED")
+	print("Chocolate Bars: ", chocolate_amount)
+	print("Cola: ", cola_amount)
+	print("Crisps", crisps_amount)
+	
+	# Reset quantities
+	$PhonePanel/ProductScroll/ProductList/ChocolateCard/ChocolateAmount.text = "0"
+	$PhonePanel/ProductScroll/ProductList/ColaCard/ColaAmount.text = "0"
+	$PhonePanel/ProductScroll/ProductList/CrispsCard/CrispsAmount.text = "0"
+	
+	# Reset total
+	$PhonePanel/OrderTotal.text = "TOTAL: £0.00"
+	
+	$PhonePanel/PlaceOrderButton.text = "ORDER PLACED"
+	$PhonePanel/PlaceOrderButton.disabled = true
